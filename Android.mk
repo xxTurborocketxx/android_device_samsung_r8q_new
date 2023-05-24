f882c29fd51d5927ed2966b4b8fa0b52a51e92c4
xxTurborocketxx <tgeywitz8@gmail.com>
2023-05-24 12:02:57 +0200

    Initial stuff

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),a52sxq)
+ifeq ($(TARGET_DEVICE),r8q)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 endif
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_a52sxq.mk
+    $(LOCAL_DIR)/twrp_r8q.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_a52sxq-eng
+    twrp_r8q-eng
